5e65157fd94b1a6d274e6093c2bac3bafe6338ca
Lano <69957752+Lano0423@users.noreply.github.com>
2021-08-22 17:52:25 +0900

    Update registerRouter.js

--- server/router/registerRouter.js
+++ server/router/registerRouter.js
@@ -1,8 +1,25 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    'host': 'localhost',
+    'port': '4000',
+    'user': 'username',
+    'password': 'PW'
+});
+
+con.connect();
+
 module.exports = function(router)
 {
-    router.post('/', function(req,res){
 
+    router.post('/register', function(req, res) {
+        let { id, pass, name, ip } = req.body;
+        let query = `SELECT * FROM USER WHERE id=${id}`;
+        con.query(query, function(err, row) {
+            if (!row[0]) return res.send(0);
+            query = 'INSERT INTO USER (name, id, pass, ip) VALUES (?, ?, ?, ?)';
+            con.query(query, [id, pass, name, ip], (err) => res.send(+!err));
+        });
     });
 
-    return router;
-} 
+}
