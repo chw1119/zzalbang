d5b277878c554250762975a3498ea828c9268cc7
Lano <69957752+Lano0423@users.noreply.github.com>
2021-08-22 18:00:47 +0900

    Update web.js

--- web.js
+++ web.js
@@ -4,8 +4,6 @@ const http = require('http');
 const router = express.Router();
 const app = express();
 
-const PORT = 8001;
-
 /*  router imports  */
 
 const imageRouter = require('./server/router/imageRouter')(router);
@@ -14,4 +12,4 @@ const registerRouter = require('./server/router/registerRouter')(router);
 app.use('/images', imageRouter);
 app.use('/register', registerRouter);
 
-app.listen(PORT);
+app.listen(8001);
