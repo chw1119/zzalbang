805604ad65c6b6edff4ae93c287888917ec537ce
Lano <yk2yd2@daum.net>
2021-08-22 17:10:40 +0900

    HI

--- server/router/register.js
+++ server/router/register.js
@@ -1,1 +1,21 @@
+const app = require("express")();
+const mysql = require("mysql");
 
+const con = mysql.createConnection({
+    "host": "localhost",
+    "port": "4000",
+    "user": "username",
+    "password": "PW"
+});
+
+con.connect();
+
+app.post("/register", function(req, res) {
+    let { id, pass, name, ip } = req.body;
+    let query = `SELECT * FROM USER WHERE id=${id}`;
+    con.query(query, function(err, row, fields) {
+        if (!row[0]) return res.send(0);
+    });
+});
+
+app.listen(5000);
