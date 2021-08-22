ead439e3c5fbfc8a965e150f1d5e40fa503ec206
Lano <yk2yd2@daum.net>
2021-08-22 17:30:26 +0900

    REGISTER

--- server/router/register.js
+++ server/router/register.js
@@ -12,9 +12,12 @@ con.connect();
 
 app.post("/register", function(req, res) {
     let { id, pass, name, ip } = req.body;
+    if (!(id && pass && name && ip)) return res.send(0);
     let query = `SELECT * FROM USER WHERE id=${id}`;
     con.query(query, function(err, row, fields) {
         if (!row[0]) return res.send(0);
+        query = "INSERT INTO USER (name, id, pass, ip) VALUES (?, ?, ?, ?)";
+        con.query(query, [id, pass, name, ip], (err) => res.send(+!err));
     });
 });
 
