02a8d8da92e178d99cb4d77638f4930aa3161edb
chw1119 <chahyunwooa@naver.com>
2021-08-23 00:35:15 +0900

    일단 커밋

--- server/module/database/mysql/type.js
+++ server/module/database/mysql/type.js
@@ -1,15 +1,53 @@
 const __type = class
 {
-    constructor()
+    constructor(typename, N)
     {
+        this.typename = typename || "INT";
+        this.N = N || 0;
+    }
 
+    toString()
+    {
+        return this.typename + "(" + N + ")";
     }
 }
+
+const CHAR = class extends __type
+{
+    constructor(N)
+    {
+        super("CHAR", N);
+    }
+
+    getTypeName()
+    {
+        return "CHAR";
+    }
+}
+
 
 const VARCHAR = class
 {
-    constructor()
+    constructor(N)
     {
-        
+        super("VARCHAR", N);
     }
 }
+
+
+const INT= class
+{
+    constructor()
+    {
+
+    }
+}
+
+const TINYINT= class
+{
+    constructor()
+    {
+
+    }
+}
+
